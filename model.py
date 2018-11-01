9ac2c9052b86507cf8d640ae74378a6a76c00cd9
yuanpeng <43242776+yuanpeng50@users.noreply.github.com>
2018-11-01 13:00:08 +0800

    Update model.py

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class Model():
         self.Y = tf.placeholder(
             tf.int32, shape=[None, self.num_steps], name='label')
 
-        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        self.keep_prob = tf.placeholder(tf.float32, name='self.keep_prob')
 
         with tf.variable_scope('embedding'):
             if embedding_file:
@@ -58,51 +58,57 @@ class Model():
 
             data = tf.nn.embedding_lookup(embed, self.X)
 
-        def get_a_cell(lstm_size, keep_prob):
-            lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
-            drop = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=keep_prob)
-            return drop
-
         with tf.variable_scope('rnn'):
-            cell = tf.nn.rnn_cell.MultiRNNCell(
-                [get_a_cell(self.dim_embedding, self.keep_prob) for _ in range(self.rnn_layers)]
-            )
 
-            self.state_tensor = cell.zero_state(self.batch_size, tf.float32)
-            outputs_tensor, final_state = tf.nn.dynamic_rnn(cell, data, initial_state=self.state_tensor)
-            self.outputs_state_tensor = final_state
+            #hidden_size=256
+            stacked_rnn = []
+            for iLayer in range(self.rnn_layers):
+                lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(128, forget_bias=1.0, state_is_tuple=True)
+                lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.keep_prob)
+                stacked_rnn.append(lstm_cell)
 
-            # concate every time step
+            cell = tf.nn.rnn_cell.MultiRNNCell(stacked_rnn, state_is_tuple=True)
+
+            self.init_state = cell.zero_state(self.batch_size, dtype=tf.float32)
+
+            #print('xxxxxxxx', self.X)
+            #print('yyyyyyyy', self.Y)
+            rnn_inputs = tf.one_hot(self.X, self.num_words)
+            #print('rnn_inputs: ', rnn_inputs.shape)
+            state = self.init_state
+            outputs_tensor, state = tf.nn.dynamic_rnn(cell, inputs=rnn_inputs, initial_state=state, time_major=False)
+            #print(outputs_tensor)
+            #print(state)
+            outputs_state_tensor = outputs_tensor[:, -1, :]
+            self.outputs_state_tensor = outputs_state_tensor
+            self.final_state = state
+
+        # concate every time step
         seq_output = tf.concat(outputs_tensor, 1)
 
         # flatten it
-        seq_output_final = tf.reshape(tf.concat(outputs_tensor, 1), [-1, self.dim_embedding])
-        print('seq_output.shape: ', seq_output.shape)
-        print('seq_output_final.shape: ', seq_output_final.shape)
+        seq_output_final = tf.reshape(seq_output, [-1, self.dim_embedding])
+        #print('seq_output.shape: ', seq_output.shape)
+        #print('seq_output_final.shape: ', seq_output_final.shape)
 
         with tf.variable_scope('softmax'):
-            W = tf.get_variable('W', [self.dim_embedding, self.num_words])
-            b = tf.get_variable('b', [self.num_words])
-
-        logits = tf.matmul(seq_output_final, W) + b
+            W = tf.get_variable('W', [128, self.num_words], 
+                                initializer=tf.random_normal_initializer(stddev=0.01))
+            bias = tf.get_variable('b', [self.num_words], initializer=tf.constant_initializer(0.0))
+            logits = tf.matmul(seq_output_final, W) + bias
+        #print('outputs_state_tensor.shape: ', outputs_state_tensor.shape)
+        #print('logits.shape: ', logits.shape)
 
         tf.summary.histogram('logits', logits)
 
         self.predictions = tf.nn.softmax(logits, name='predictions')
 
         y_one_hot = tf.one_hot(self.Y, self.num_words)
-        print('y_one_hot.shape: ', y_one_hot.shape)
+        #print('y_one_hot.shape: ', y_one_hot.shape)
         y_reshaped = tf.reshape(y_one_hot, logits.get_shape())
-
-        loss = tf.nn.softmax_cross_entropy_with_logits(labels=y_reshaped, logits=logits)
-        mean, var = tf.nn.moments(logits, -1)
+        loss = tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits, labels=y_reshaped)
         self.loss = tf.reduce_mean(loss)
-        tf.summary.scalar('logits_loss', self.loss)
-
-        var_loss = tf.divide(10.0, 1.0 + tf.reduce_mean(var))
-        tf.summary.scalar('var_loss', var_loss)
-        self.loss = self.loss + var_loss
-        tf.summary.scalar('total_loss', self.loss)
 
         # gradient clip
         tvars = tf.trainable_variables()
@@ -112,5 +118,5 @@ class Model():
             zip(grads, tvars), global_step=self.global_step)
 
         tf.summary.scalar('loss', self.loss)
-
+        
         self.merged_summary_op = tf.summary.merge_all()
