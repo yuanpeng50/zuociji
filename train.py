ac2f4d948237bedb39439c9ef3b13f8d21a55083
yuanpeng <43242776+yuanpeng50@users.noreply.github.com>
2018-11-01 13:16:39 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,14 +12,16 @@ from model import Model
 from utils import read_data
 
 from flags import parse_args
-
 FLAGS, unparsed = parse_args()
+
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s', level=logging.DEBUG)
 
+
 vocabulary = read_data(FLAGS.text)
 print('Data size', len(vocabulary))
+
 
 with open(FLAGS.dictionary, encoding='utf-8') as inf:
     dictionary = json.load(inf, encoding='utf-8')
@@ -27,8 +29,10 @@ with open(FLAGS.dictionary, encoding='utf-8') as inf:
 with open(FLAGS.reverse_dictionary, encoding='utf-8') as inf:
     reverse_dictionary = json.load(inf, encoding='utf-8')
 
+
 model = Model(learning_rate=FLAGS.learning_rate, batch_size=FLAGS.batch_size, num_steps=FLAGS.num_steps)
-model.build()
+model.build("embedding.npy")
+
 
 with tf.Session() as sess:
     summary_string_writer = tf.summary.FileWriter(FLAGS.output_dir, sess.graph)
@@ -44,21 +48,20 @@ with tf.Session() as sess:
         logging.debug('restore from [{0}]'.format(checkpoint_path))
 
     except Exception:
-        logging.debug('no check point found in [{0}]'.format(FLAGS.output_dir))
-        checkpoint_path = FLAGS.restore_constant_checkout
-        saver.restore(sess, checkpoint_path)
-        logging.debug('restore from [{0}]'.format(checkpoint_path))
+        logging.debug('no check point found....')
 
     for x in range(1):
         logging.debug('epoch [{0}]....'.format(x))
-        # state = sess.run(model.state_tensor)
+        state = sess.run(model.init_state)
         for dl in utils.get_train_data(vocabulary, dictionary, batch_size=FLAGS.batch_size, num_steps=FLAGS.num_steps):
-
-            feed_dict = {model.X: dl[0], model.Y: dl[1], model.keep_prob: FLAGS.keep_prob}
+            feed_dict={}
+            feed_dict[model.X] = dl[0]
+            feed_dict[model.Y] = dl[1]
+            feed_dict[model.keep_prob] = 0.9
+            feed_dict[model.init_state] = state
 
             gs, _, state, l, summary_string = sess.run(
-                [model.global_step, model.optimizer, model.outputs_state_tensor, model.loss, model.merged_summary_op],
-                feed_dict=feed_dict)
+                [model.global_step, model.optimizer, model.final_state, model.loss, model.merged_summary_op], feed_dict=feed_dict)
             summary_string_writer.add_summary(summary_string, gs)
 
             if gs % 10 == 0:
